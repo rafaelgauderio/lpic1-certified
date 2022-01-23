22d27c4ee4cc6796ef0e9317c894edaa507e6113
Rafael De Luca <deluca1712@gmail.com>
2022-01-23 12:48:02 -0300

    107.1 usseradd, userdel, passwd

---
+++ 107.1. Manage user and group accounts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+sudo su -           # give tourself root permissions
+useradd rafael      # create a nem user or update the default new user information
+su - rafael         # try to log in as rafael, will give a error message
+userdel rafael      # delete the user
+useradd -u 1500 -d /home/rafael -m -s /bin/bash -c "Test user" rafael
+    #-u= user id 1500, -d=home directory, -m=home directory created, -s=shell, -c=comment
+passwd rafael       # create a password, ever user can change his own password
+
+ll /etc/skel        # directory that contains the files .bash_logout, .bashrc, .profile
+                    # this files are automatically create in the home directory from the new user
+mkdir /etc/skel2
+touch .bashrcLuciana
+useradd -u 1501 -d /home/luciana -k /etc/skel2 -m -s /bin/ksh
+su - luciana        # log in as luciana
+ls -la              # see files from /etc/luciana
+
+vi /etc/passwd      # see all users from the system
+
+chsh -s /bin/false luciana   # change sthe shell user
+
+
+
+
+
