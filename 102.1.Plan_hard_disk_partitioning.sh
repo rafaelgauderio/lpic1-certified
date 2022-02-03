dc5713849ba0d4a5811d7c461de59f185a9fbfde
Rafael de Luca <deluca1712@gmail.com>
2022-02-03 14:36:12 -0300

    102.1. Hard disk partition

--- 102.1.Plan_hard_disk_partitioning.sh
+++ 102.1.Plan_hard_disk_partitioning.sh
@@ -1,3 +1,4 @@
+<<Directories
 -> 102.1. PLAN HARD DISK PARTITIONING
 
 	-> DIRECTORY STRUCTURE ACCORDING TO FHS
@@ -21,63 +22,69 @@
 					-> /bin, /lib, /local, /share, /share/doc
 		/var 	= data that is constantly changing, log files
 					-> /www
+Directories
 
-	 # cd / 	= go to root directory
-	 # cd /usr/share/doc 	= documentations files 
+cd / 					# go to root directory
+cd /usr/share/doc 		# documentations files 
 
 
-	------------------------------------------
+<<Swap
 	-> SWAP
 		-> If the main memory is sufficient for all data, the SWAP partions remain empty.
 		-> Whem memory is full, the last need data from memory will go to SWAP partition.
 		-> swap file is significant slower than a swap partition.
 		-> hardly used nowdays.
+Swap
 
-	# man swapon 		= enable/disable devices and files for paging and swapping
-	# swapon -s 		= see the swap partition/file the we are using
-	# cat /proc/swaps
-	# sudo swapoff -a 	= disable all swap partitions and files
-	# sudo swapon -a 	= enable all swap partitions and files
-	# vi /etc/fstab
+man swapon 				# enable/disable devices and files for paging and swapping
+swapon -s 				# see the swap partition/file the we are using
+cat /proc/swaps			
+sudo swapoff -a 		# disable all swap partitions and files
+sudo swapon -a 			# enable all swap partitions and files
+vi /etc/fstab
 
-	------------------------------------------
+
+<<Partitions
 	-> PARTITIONS
 		-> HD = extends for Hard Disk
 			-> HDA, HDB
 		-> Today
 			-> SDA, SDB, SDC = CSI Hard Disk
-	# cd /dev
-	# ll
 		-> only possible 4 PRIMARY PARTITIONS
 			-> sda1, sda2, sdb3, sdb4
 		-> can create a EXTEND PARTITION in a primary partitions
-		-> in the extend partition we can create several LOGICAL DRIVES
+		-> in the extend partition we can create SEVERAL LOGICAL DRIVES
 		-> Fist logical partition is ALWAYS sda5.
-	# cat /proc/partitions 		= display the partitions
-	# lsblk 		= display the partitions
-	# man df 		= report file system space usage
-					= correspondig hard drivers and filesystems, size used and available
-	# df -h 		= human readable
-		-> UUID 	= Unoversally Unique Identifier
-	# sudo blkid 	= display UUID
-	# lsblk -o --output list NAME,SIZE,FSTYPE,MOUNTPOINT,UUID = 	display the specific columns
+		-> UUID 	= Universally Unique Identifier
 		-> EFI  	= Extensible Firmware Interface
 		-> UEFI 	- Unified Extensible Firmware Interfabe = substitute de BIOS
+Partitions
 
+cd /dev
+ll
+cat /proc/partitions 			# display the partitions
+lsblk 							# display the partitions
+man df 							# report file system space usage
+								# correspondig hard drivers and filesystems, size used and available
+df -h 							# df --human-readable
+		
+sudo blkid 						# display UUID
+lsblk -o 						# lsblk --output list NAME,SIZE,FSTYPE,MOUNTPOINT,UUID = display the specific columns
+	
 
-	------------------------------------------
-	-> MOUNT
-		-> We have manually mount the new devices, like a cd room drive
-		-> Use differents partitions to create var and log files, because if the partition get fill up
-				the partition that contains the filesystem will not crashed.
-	# cd /etc/udev/rules.d 		= configuration files that deffined rules
-	# man mount 				= mount a filesystem 
-	# mount 					= display all the mounted filesystem, attached systems
-	# cat /proc/mounts 			= display all the mounted filesystem, attached systems
-	# cat /proc/self/mounts 	= display all the mounted filesystem, attached systems
+	# MOUNT
+		# We have manually mount the new devices, like a cd room drive
+		# Use differents partitions to create var and log files, because if the partition get fill up
+				# the partition that contains the filesystem will not crashed.
 
-	------------------------------------------
-	-> LVM
+cd /etc/udev/rules.d 			# configuration files that deffined rules
+man mount 						# mount a filesystem 
+mount 							# display all the mounted filesystem, attached systems
+cat /proc/mounts 				# display all the mounted filesystem, attached systems
+cat /proc/self/mounts 			# display all the mounted filesystem, attached systems
+
+	
+<<LVM
 		-> LOGICAL VOLUME MANAGE
 		-> fylesystem			/			/HOME 		/tmp
 		-> logical VOLUME 		LV_Root		LV_home 	LV_tmp
@@ -88,10 +95,11 @@
 				done because of LVM
 		-> LVM can be flexibly adjusted in size
 		-> can be create snapshots of a LVM and use as backup
+LVM
 
-	# sudo pvs 		= physical volumes
-	# sudo vgs 		= volume groups
-	# sudo lvs 		= logical volumes
+sudo pvs 						# physical volumes
+sudo vgs 						# volume groups
+sudo lvs 						# logical volumes
 
 
 
